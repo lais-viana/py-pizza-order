c07f21ff1d94cff453949d0386156f9cd9416f6a
Laís Viana <vianaalveslais@gmail.com>
2025-04-18 20:50:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
 extra_cheese = input("Do you want extra cheese? Y or N: ")
 bill = 0
 
+# Small pizza (S): $15
+# Medium pizza (M): $20
+# Large pizza (L): $25
+# Add pepperoni for small pizza (Y or N): +$2
+# Add pepperoni for medium or large pizza (Y or N): +$3
+
 if size == "S":
     bill += 15
     if pepperoni == "Y":
@@ -16,14 +22,10 @@ elif size == "L":
     bill += 25
     if pepperoni == "Y":
         bill += 3
+else:
+    print("You typed the wrong inputs.")
 
 if extra_cheese == "Y":
     bill += 1
 
-print(f"Your total is ${bill}.")
-
-# Small pizza (S): $15
-# Medium pizza (M): $20
-# Large pizza (L): $25
-# Add pepperoni for small pizza (Y or N): +$2
-# Add pepperoni for medium or large pizza (Y or N): +$3
+print(f"Your final bill is: ${bill}.")
